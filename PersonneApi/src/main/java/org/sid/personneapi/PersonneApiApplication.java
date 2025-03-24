3a1798dafad2e180097aea05baefc66b8169b368
dalal123334 <136986867+dalal123334@users.noreply.github.com>
2025-03-24 22:06:54 +0000

    solved apis

--- PersonneApi/src/main/java/org/sid/personneapi/PersonneApiApplication.java
+++ PersonneApi/src/main/java/org/sid/personneapi/PersonneApiApplication.java
@@ -1,32 +1,20 @@
 package org.sid.personneapi;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.web.bind.annotation.RestController;
 
 
 @SpringBootApplication
+@EnableJpaRepositories("org.sid.personnecore.repository")
+@EntityScan("org.sid.personnecore.module")
 @ComponentScan(basePackages = {"org.sid.personneapi", "org.sid.personnecore"})
-@EnableJpaRepositories(basePackages = "org.sid.personnecore.repository")
 public class PersonneApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PersonneApiApplication.class, args);
-    }
-
-    @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-        return args -> {
-            System.out.println("Controllers loaded by Spring:");
-            ctx.getBeansOfType(RestController.class).forEach((name, bean) -> {
-                System.out.println(name + " of type " + bean.getClass());
-            });
-        };
     }
 
 
